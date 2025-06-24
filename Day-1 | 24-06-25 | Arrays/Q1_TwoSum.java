5d4d5fc7c678510b948e3e600eded7649547b02f
Vaibhav Kaushal <vaibhavkaushal@Vaibhavs-MacBook-Pro.local>
2025-06-24 09:39:30 +0530

    Intial Setup

---
+++ Day-1 | 24-06-25 | Arrays/Q1_TwoSum.java
@@ -0,0 +1,16 @@
+// Q1_TwoSum.java
+
+public class Q1_TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        // Your logic here
+        return new int[2];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
