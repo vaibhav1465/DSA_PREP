b7cb44968460c8a5738bf407f48bd571606915c8
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-06-26 00:04:10 +0530

    Boiler Plate Code Added

--- Day-2 | 25-06-25 | Arrays/Q3_SmallestPositiveMissing.java
+++ Day-2 | 25-06-25 | Arrays/Q3_SmallestPositiveMissing.java
@@ -21,5 +21,20 @@ public class Q3_SmallestPositiveMissing {
 
     public static void main(String[] args) {
         
+        // Examples:
+        // Input: arr[] = [2, -3, 4, 1, 1, 7]
+        // Output: 3
+        // Explanation: Smallest positive missing number is 3.
+        // Input: arr[] = [5, 3, 2, 5, 1]
+        // Output: 4
+        // Explanation: Smallest positive missing number is 4.
+        // Input: arr[] = [-8, 0, -1, -4, -3]
+        // Output: 1
+        // Explanation: Smallest positive missing number is 1.
+
+        int[] nums = {};
+        int result = functionName(nums);
+        System.out.println("result "+result);
+
     }
 }
---
+++ boilerPlateCodeForArrayQues.java
@@ -0,0 +1,19 @@
+public class boilerPlateCodeForArrayQues {
+    
+    public static int functionName(int[] nums){
+
+       int n= nums.length;
+        for(int i=0;i<n;i++){
+            
+        }
+        return n;
+    }
+
+
+    public static void main(String[] args) {
+        
+        int[] nums = {};
+        int result = functionName(nums);
+        System.out.println("result "+result);
+    }
+}
