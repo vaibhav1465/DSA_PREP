db88a91377f584267ce2ad21b4bccba550ba49ae
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-06-26 11:42:04 +0530

    Sort 0s 1s And 2s in Array using One Traversal

--- Day-3 | 26-06-25 | Arrays/Q1_Sort0s1s2sInArray.java
+++ Day-3 | 26-06-25 | Arrays/Q1_Sort0s1s2sInArray.java
@@ -1,16 +1,34 @@
 public class Q1_Sort0s1s2sInArray {
 
-      public static void sort0s1s2sInArray(int[] nums){
+    public static void sort0s1s2sInArray(int[] nums) {
 
-       int n= nums.length;
-        for(int i=0;i<n;i++){
-            
+        int n = nums.length;
+        int m = 0;
+        int h = n - 1;
+        for (int i = 0; i <= h; i++) {
+            // System.out.println(nums[i]);
+            if (nums[i] == 0) {
+                int t = nums[i];
+                nums[i] = nums[m];
+                nums[m] = t;
+                m++;
+            } else if (nums[i] == 1) {
+                continue;
+            } else if (nums[i] == 2) {
+                int t = nums[i];
+                nums[i] = nums[h];
+                nums[h] = t;
+                h--;
+                i--;
+            }
+            // for (int j = 0; j < n; j++) {
+            //     System.out.print(nums[j] + " ");
+            // }
+            // System.out.println();
         }
     }
 
-
     public static void main(String[] args) {
-        
 
         // Examples:
         // Input: arr[] = [0, 1, 2, 0, 1, 2]
@@ -19,11 +37,17 @@ public class Q1_Sort0s1s2sInArray {
         // Input: arr[] = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
         // Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
         // Explanation: 0s 1s and 2s are segregated into ascending order.
-        // Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+        // Follow up: Could you come up with a one-pass algorithm using only constant
+        // extra space?
 
-
-        int[] nums = {};
+        int[] nums = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 }; // using Dutch National Flag Algorithm
         sort0s1s2sInArray(nums);
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+
     }
-    
+
 }
