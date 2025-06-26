7ef2ed4c3eb50bde72a570f0376e9ed66e197dff
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-06-26 11:20:49 +0530

    Day 3 Added

---
+++ Day-3 | 26-06-25 | Arrays/Q1_Sort0s1s2sInArray.java
@@ -0,0 +1,29 @@
+public class Q1_Sort0s1s2sInArray {
+
+      public static void sort0s1s2sInArray(int[] nums){
+
+       int n= nums.length;
+        for(int i=0;i<n;i++){
+            
+        }
+    }
+
+
+    public static void main(String[] args) {
+        
+
+        // Examples:
+        // Input: arr[] = [0, 1, 2, 0, 1, 2]
+        // Output: [0, 0, 1, 1, 2, 2]
+        // Explanation: 0s 1s and 2s are segregated into ascending order.
+        // Input: arr[] = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+        // Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
+        // Explanation: 0s 1s and 2s are segregated into ascending order.
+        // Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+
+
+        int[] nums = {};
+        sort0s1s2sInArray(nums);
+    }
+    
+}
