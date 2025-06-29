c32a640533b8d0f7c776fd4d5d2cbc4bd6a2f4b8
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-06-30 00:19:28 +0530

    Median of 2 Sorted Arrays of Different Sizes

--- Day-7 | 30-06-25 | Arrays/Q6_MedianOfTwoSortedArraysOfDifferentSizes.java
+++ Day-7 | 30-06-25 | Arrays/Q6_MedianOfTwoSortedArraysOfDifferentSizes.java
@@ -3,6 +3,8 @@ public class Q6_MedianOfTwoSortedArraysOfDifferentSizes {
 
     public static double medianOf2(int a[], int b[]) {
         // Your Code Here
+
+        return 0.0;
         
     }
     public static void main(String[] args) {
