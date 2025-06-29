3f163f969b2719afbd993f7244eada31b77719f6
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-06-30 00:12:16 +0530

    Minimum swaps and K together

---
+++ Day-7 | 30-06-25 | Arrays/Q3_MinimumSwapsSndKtogether.java
@@ -0,0 +1,11 @@
+
+public class Q3_MinimumSwapsSndKtogether {
+ 
+    public static int minSwap(int[] arr, int k) {
+        return k;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
