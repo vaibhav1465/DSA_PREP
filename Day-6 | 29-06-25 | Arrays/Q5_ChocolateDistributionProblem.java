5ac19d9b168d807f88b5f6431672454a2778c5bb
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-06-29 23:55:41 +0530

    Chocolate Distribution Problem

---
+++ Day-6 | 29-06-25 | Arrays/Q5_ChocolateDistributionProblem.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Q5_ChocolateDistributionProblem {
+    public static int findMinDiff(ArrayList<Integer> arr, int m) {
+        // your code here
+
+        return m;
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
