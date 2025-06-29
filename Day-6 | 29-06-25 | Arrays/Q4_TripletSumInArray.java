fa10e84052e1080f965890ce38be5afe17d75d59
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-06-29 23:51:50 +0530

    Triplet Sum in Array

---
+++ Day-6 | 29-06-25 | Arrays/Q4_TripletSumInArray.java
@@ -0,0 +1,14 @@
+public class Q4_TripletSumInArray {
+    
+    // Should return true if there is a triplet with sum equal
+    // to x in arr[], otherwise false
+    public static boolean hasTripletSum(int arr[], int target) {
+        // Your code Here
+
+        return false;
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
