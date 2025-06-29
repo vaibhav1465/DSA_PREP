236fdfa0b0441f1578af54ed5f43b0587edbaf05
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-06-30 00:10:18 +0530

    Three way partitioning

---
+++ Day-7 | 30-06-25 | Arrays/Q2_ThreeWayPartitioning.java
@@ -0,0 +1,15 @@
+
+public class Q2_ThreeWayPartitioning {
+
+     // Function to partition the array around the range such
+    // that array is divided into three parts.
+    public void threeWayPartition(int arr[], int a, int b) {
+        // code here
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
+
+
