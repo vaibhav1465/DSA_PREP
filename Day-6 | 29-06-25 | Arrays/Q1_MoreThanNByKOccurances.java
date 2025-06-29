674fdb4c6b1fb96306a4fe128b4f338f0a896663
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-06-29 23:41:08 +0530

    More than n/k Occurrences

---
+++ Day-6 | 29-06-25 | Arrays/Q1_MoreThanNByKOccurances.java
@@ -0,0 +1,12 @@
+public class Q1_MoreThanNByKOccurances {
+
+     // Function to find all elements in array that appear more than n/k times.
+    public static int countOccurence(int[] arr, int k) {
+        // your code here,return the answer
+
+        return arr.length;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
