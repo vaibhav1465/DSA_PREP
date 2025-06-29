71a802853717e99aef4d9b8cb6bf18fcbd2a2277
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-06-30 00:08:24 +0530

    Smallest subarray with sum greater than x

---
+++ Day-7 | 30-06-25 | Arrays/Q1_SmallestSubarrayWithSumGreaterThanX.java
@@ -0,0 +1,16 @@
+public class Q1_SmallestSubarrayWithSumGreaterThanX {
+
+    
+    public static int smallestSubWithSum(int x, int[] arr) {
+        // Your code goes here
+
+        return arr.length;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
+
+// Smallest subarray with sum greater than x
