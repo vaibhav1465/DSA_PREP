74a58800af090fadf2540c5981b0159e980c52de
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-06-30 00:15:59 +0530

    Median of an Array

--- Day-7 | 30-06-25 | Arrays/Q5_MedianOfAnArray.java
+++ Day-7 | 30-06-25 | Arrays/Q5_MedianOfAnArray.java
@@ -3,6 +3,7 @@ public class Q5_MedianOfAnArray {
 
     public static double findMedian(int[] arr) {
         // Code here.
+        return 0.0;
     }
     public static void main(String[] args) {
         
