890718cdbcc6e83408e94fdf56405aed82f62b10
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-02 01:58:44 +0530

    Median of an Array

--- Day-7 | 30-06-25 | Arrays/Q4_PalindromicArray.java
+++ Day-7 | 30-06-25 | Arrays/Q4_PalindromicArray.java
@@ -2,10 +2,44 @@
 public class Q4_PalindromicArray {
 
     public static boolean isPalinArray(int[] arr) {
+
+        for(int i=0;i<arr.length;i++){
+
+        int temp = arr[i];
+
+        int rev = 0;
+        while (temp>0) {
+            int rem = temp%10;
+            rev = rev * 10 + rem;
+            temp = temp/10;  
+        }
+        System.out.println(rev+" "+arr[i]);
+        if(rev != arr[i]){
+         return false;
+        }
+        }
+
         // add code here.
-        return false;
+        return true;
     }
     public static void main(String[] args) {
         //Palindromic Array
+        // Input: arr[] = [111, 222, 333, 444, 555]
+        // Output: true
+        // Explanation:
+        // arr[0] = 111, which is a palindrome number.
+        // arr[1] = 222, which is a palindrome number.
+        // arr[2] = 333, which is a palindrome number.
+        // arr[3] = 444, which is a palindrome number.
+        // arr[4] = 555, which is a palindrome number.
+        // As all numbers are palindrome so This will return true.
+        // Input: arr[] = [121, 131, 20]
+        // Output: false
+        // Explanation: 20 is not a palindrome hence the output is false.
+
+        int[] nums = {111, 222, 333, 444, 555};
+        boolean result = isPalinArray(nums);
+        System.out.println("result "+result);
+
     }
 }
--- Day-7 | 30-06-25 | Arrays/Q5_MedianOfAnArray.java
+++ Day-7 | 30-06-25 | Arrays/Q5_MedianOfAnArray.java
@@ -1,12 +1,38 @@
+import java.util.Arrays;
 
 public class Q5_MedianOfAnArray {
 
     public static double findMedian(int[] arr) {
         // Code here.
-        return 0.0;
+        Arrays.sort(arr);
+        int n=arr.length;
+        System.out.println(arr[(n/2)-1]);
+        System.out.println(arr[(n/2)]);
+        System.out.println(arr[(n/2)-1]+arr[(n/2)]);
+        if(n % 2 == 0){
+            return  (arr[(n/2)-1] + arr[n/2])/2.0;
+        }else{
+            return arr[n/2];
+        }
     }
     public static void main(String[] args) {
         // Median of an Array
+        // Examples:
+        // Input: arr[] = [90, 100, 78, 89, 67]
+        // Output: 89
+        // Explanation: After sorting the array middle element is the median 
+        // Input: arr[] = [56, 67, 30, 79]
+        // Output: 61.5
+        // Explanation: In case of even number of elements, average of two middle elements is the median. 
+        // Input: arr[] = [1, 2]
+        // Output: 1.5
+        // Explanation: The average of both elements will result in 1.5.
+
+        int[] nums = {56, 67, 30, 79};
+        double result = findMedian(nums);
+        System.out.println("result "+result);
+
+
     }
 }
 
