4237e3f55f8ba778cb66d9e46a0e2b039eb91ff4
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-03 23:27:23 +0530

    Rearrange Array alternatively

--- Day-4 | 27-06-25 | Arrays/Q5_RearrangeArrayAlternately.java
+++ Day-4 | 27-06-25 | Arrays/Q5_RearrangeArrayAlternately.java
@@ -125,7 +125,7 @@ public class Q5_RearrangeArrayAlternately {
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + " ");
         }
-        // System.out.println();
+        System.out.println();
     }
 
 }
