0a77c58187adea5104d83583aad21d74f2c54a5d
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-04 09:35:14 +0530

    Triplet Sum in Array

--- Day-6 | 29-06-25 | Arrays/Q4_TripletSumInArray.java
+++ Day-6 | 29-06-25 | Arrays/Q4_TripletSumInArray.java
@@ -10,5 +10,17 @@ public class Q4_TripletSumInArray {
     }
     public static void main(String[] args) {
         
+    
+
+        // Examples
+        // Input: arr[] = [1, 4, 45, 6, 10, 8], target = 13 
+        // Output: true 
+        // Explanation: The triplet {1, 4, 8} sums up to 13
+        // Input: arr[] = [1, 2, 4, 3, 6, 7], target = 10 
+        // Output: true 
+        // Explanation: The triplets {1, 3, 6} and {1, 2, 7} both sum to 10. 
+        // Input: arr[] = [40, 20, 10, 3, 6, 7], target = 24 
+        // Output: false 
+        // Explanation: No triplet in the array sums to 24
     }
 }
