036ceb8048a60782518744fc2c9e527ef29a8abe
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-06 23:43:11 +0530

    Minimum Jump

--- Day-5 | 28-06-25 | Arrays/Q9_MinimumJump.java
+++ Day-5 | 28-06-25 | Arrays/Q9_MinimumJump.java
@@ -3,7 +3,7 @@ public class Q9_MinimumJump {
 
     public static int minJumps(int[] arr) {
         // code here
-        
+        return arr.length;
     }
     public static void main(String[] args) {
         // Minimum Jump
