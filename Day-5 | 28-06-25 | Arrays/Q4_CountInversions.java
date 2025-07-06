6cfc6514e2b0d357d06fc8f090e6da8f28303cf6
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-06 23:45:54 +0530

    Count Inversions

--- Day-5 | 28-06-25 | Arrays/Q4_CountInversions.java
+++ Day-5 | 28-06-25 | Arrays/Q4_CountInversions.java
@@ -1,5 +1,11 @@
 public class Q4_CountInversions {
 
+    // Function to count inversions in the array.
+    static int inversionCount(int arr[]) {
+        // Your Code Here
+        
+    }
+
     public static void main(String[] args) {
         // Count Inversions
     }
