f0d00b24deab0136cca75a07c4681ccaf3b23c04
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-06 23:44:54 +0530

    Merge Without Extra Space

--- Day-5 | 28-06-25 | Arrays/Q7_MergeWithoutExtraSpace.java
+++ Day-5 | 28-06-25 | Arrays/Q7_MergeWithoutExtraSpace.java
@@ -1,5 +1,10 @@
 public class Q7_MergeWithoutExtraSpace {
  
+    // Function to merge the arrays.
+    public void mergeArrays(int a[], int b[]) {
+        // code here
+        
+    }
     public static void main(String[] args) {
         // Merge Without Extra Space
     }
