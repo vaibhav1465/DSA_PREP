25ce8d64859074299d98982cbaca9eeb3d99f9ed
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-10 00:26:39 +0530

    Stock Buy and Sell – Max K Transactions Allowed

--- Day-6 | 29-06-25 | Arrays/Q2_StockBuyAndSellMaxKTransaction.java
+++ Day-6 | 29-06-25 | Arrays/Q2_StockBuyAndSellMaxKTransaction.java
@@ -15,5 +15,24 @@ public class Q2_StockBuyAndSellMaxKTransaction {
     }
     public static void main(String[] args) {
         // Stock Buy and Sell – Max K Transactions Allowed
+        // Examples :
+        // Input: prices[] = [10, 22, 5, 80], k = 2
+        // Output: 87
+        // Explaination:
+        // 1st transaction: Buy at 10 and sell at 22. 
+        // 2nd transaction : Buy at 5 and sell at 80.
+        // Total Profit will be 12 + 75 = 87.
+        // Input: prices[] = [20, 580, 420, 900], k = 3
+        // Output: 1040
+        // Explaination: 
+        // 1st transaction: Buy at 20 and sell at 580. 
+        // 2nd transaction : Buy at 420 and sell at 900.
+        // Total Profit will be 560 + 480 = 1040.
+        // Input: prices[] = [100, 90, 80, 50, 25],  k = 1
+        // Output: 0
+        // Explaination: Selling price is decreasing continuously
+        // leading to loss. So seller cannot have any profit.
+
+        
     }
 }
