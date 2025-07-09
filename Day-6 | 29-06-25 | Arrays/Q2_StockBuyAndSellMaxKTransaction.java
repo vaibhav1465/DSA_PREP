5fd31dba9c67e49f0a79f635c41bb71838246a3d
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-10 00:25:44 +0530

    Stock Buy and Sell – Max K Transactions Allowed

--- Day-6 | 29-06-25 | Arrays/Q2_StockBuyAndSellMaxKTransaction.java
+++ Day-6 | 29-06-25 | Arrays/Q2_StockBuyAndSellMaxKTransaction.java
@@ -1,8 +1,17 @@
 public class Q2_StockBuyAndSellMaxKTransaction {
-    public static int maxProfit(int prices[], int k) {
+    public static int maxProfit(int arr[], int k) {
         // code here
+
+        int profit = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] < arr[i]) {
+                profit += arr[i] - arr[i - 1];
+            }
+        }
+
+        return profit;
         
-        return prices.length;
     }
     public static void main(String[] args) {
         // Stock Buy and Sell – Max K Transactions Allowed
