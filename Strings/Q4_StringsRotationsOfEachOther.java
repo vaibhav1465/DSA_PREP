5534a910671fcb7aa830c76a441822e3261c4cb0
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-11 22:59:23 +0530

    Strings Rotations of Each Other

---
+++ Strings/Q4_StringsRotationsOfEachOther.java
@@ -0,0 +1,34 @@
+public class Q4_StringsRotationsOfEachOther {
+
+    public static boolean areRotations(String s1, String s2) {
+        // code here
+
+        for(int i=0;i<s1.length();i++){
+            String temp = s1.substring(1) + s1.charAt(0);
+            if(s2.equals(temp)){
+                return true;
+            }
+            s1=temp;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        // Input: s1 = "abcd", s2 = "cdab"
+        // Output: true
+        // Explanation: After 2 right rotations, s1 will become equal to s2.
+        // Input: s1 = "aab", s2 = "aba"
+        // Output: true
+        // Explanation: After 1 left rotation, s1 will become equal to s2.
+        // Input: s1 = "abcd", s2 = "acbd"
+        // Output: false
+        // Explanation: Strings are not rotations of each other
+
+        String s1 = "abcd";
+        String s2 = "cdab";
+        boolean result = areRotations(s1, s2);
+        System.out.println(result);
+    }
+}
