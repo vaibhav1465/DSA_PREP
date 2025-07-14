805f7986c89e5f6aa811329759597607ba5d0b60
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-14 23:31:28 +0530

    Split the binary string into substrings with equal number of 0s and 1s

--- Strings/Q12_SplitBinaryString.java
+++ Strings/Q12_SplitBinaryString.java
@@ -1,3 +1,22 @@
 public class Q12_SplitBinaryString {
-    
+
+     public static int maxSubStr(String str) {
+        // Write your code here
+        
+        return str.length();
+    }
+
+    public static void main(String[] args) {
+        // Example 1:
+        // Input:
+        // S = "0100110101"
+        // Output: 4
+        // Explanation: 
+        // The required substrings are 01, 0011, 01 and 01.
+
+        // Example 2:
+        // Input:
+        // S = "0111100010"
+        // Output: 3
+    }
 }
