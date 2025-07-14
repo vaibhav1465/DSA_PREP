16c91e711201dda11995e6bde380362a9d2db3a6
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-14 23:16:03 +0530

    LongestRepeatingSubsequence

---
+++ Strings/Q9_LongestRepeatingSubsequence.java
@@ -0,0 +1,11 @@
+public class Q9_LongestRepeatingSubsequence {
+
+    public static int LongestRepeatingSubsequence(String s) {
+        // code here
+        return s.length();
+    }
+    public static void main(String[] args) {
+        //Longest Repeating Subsequence
+
+    }
+}
