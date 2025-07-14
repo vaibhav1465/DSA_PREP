7d38f75ae48eaa71e915847b57325eef6af93332
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-14 23:29:02 +0530

    Permutations of a String

--- Strings/Q11_PermutationsOfString.java
+++ Strings/Q11_PermutationsOfString.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+
 public class Q11_PermutationsOfString {
+
+    public static ArrayList<String> findPermutation(String s) {
+        // Code here
+        
+    }
     public static void main(String[] args) {
         
+        // Input: s = "ABC"
+        // Output: ["ABC", "ACB", "BAC", "BCA", "CAB", "CBA"]
+        // Explanation: Given string ABC has 6 unique permutations.
+        // Input: s = "ABSG"
+        // Output: ["ABGS", "ABSG", "AGBS", "AGSB", "ASBG", "ASGB", "BAGS", "BASG", "BGAS", "BGSA", "BSAG", "BSGA", "GABS", "GASB", "GBAS", "GBSA", "GSAB", "GSBA", "SABG", "SAGB", "SBAG", "SBGA", "SGAB", "SGBA"]
+        // Explanation: Given string ABSG has 24 unique permutations.
+        // Input: s = "AAA"
+        // Output: ["AAA"]
+        // Explanation: No other unique permutations can be formed as all the characters are same.
+
     }
 }
