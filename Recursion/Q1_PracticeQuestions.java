97b3a3c7cc81fecdd5daaad377ce0af43a44f680
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-19 23:48:32 +0530

    Recursion Basic Question

---
+++ Recursion/Q1_PracticeQuestions.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class Q1_PracticeQuestions {
+    public static void print1ToN(){
+
+    }
+    // public static void printNTo1(){
+
+    // }
+    // public static void print1ToNBackTracking(){
+
+    // }
+    // public static void printNToNBackTracking(){
+
+    // }
+    public static void main(String[] args) {
+        // Recursion
+    }
+}
