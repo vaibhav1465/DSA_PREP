41abc3c7f4869ddda76ef5a3f81637519dcbfc6d
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-22 11:42:36 +0530

    Print All Subsquence Of Array

---
+++ Recursion/Q3_PrintAllSubsequenceOfArray.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+import java.util.ArrayList;
+
+public class Q3_PrintAllSubsequenceOfArray {
+    public static ArrayList<ArrayList<Integer>> printAllSubsequenceOfArray(ArrayList<ArrayList<Integer>> res){
+        return  res;
+    }
+    public static void main(String[] args) {
+
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        printAllSubsequenceOfArray(result);
+    }
+}
