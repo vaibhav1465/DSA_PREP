95db57c2e9a499248be072c4778b9ab986bc5003
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-22 12:11:46 +0530

    Print All Subsquence Of Array

--- Recursion/Q3_PrintAllSubsequenceOfArray.java
+++ Recursion/Q3_PrintAllSubsequenceOfArray.java
@@ -3,7 +3,27 @@ package Recursion;
 import java.util.ArrayList;
 
 public class Q3_PrintAllSubsequenceOfArray {
-    public static void printAllSubsequenceOfArray(ArrayList<ArrayList<Integer>> res){
+    public static void printAllSubsequenceOfArray(ArrayList<ArrayList<Integer>> res,int[] a,int ind,ArrayList<Integer> helper){
+
+        if(ind >= a.length){
+            res.add(new ArrayList<>(helper));
+//            System.out.println("here--------------------"+res+"---"+ind);
+            return;
+        }
+
+        // Pick value at idx
+        helper.add(a[ind]);
+//        System.out.println(helper+"----------"+ind);
+        printAllSubsequenceOfArray(res,a,ind+1,helper);
+
+//        System.out.println(ind);
+        // remove value for futher
+        helper.remove(helper.size()-1);
+
+//        System.out.println(helper+"----after remove------"+ind);
+
+        // Not pick value at x
+        printAllSubsequenceOfArray(res,a,ind+1,helper);
     }
     public static void printOneSubsequenceOfArray(ArrayList<Integer> res){
     }
@@ -12,15 +32,19 @@ public class Q3_PrintAllSubsequenceOfArray {
     }
     public static void main(String[] args) {
 
+        int[] a = new int[]{1,2,3};
+
         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-        printAllSubsequenceOfArray(result);
+        ArrayList<Integer> helper =new ArrayList<>();
+        printAllSubsequenceOfArray(result,a,0,helper);
+        System.out.println(result);
 
 
-        ArrayList<Integer> result2 = new ArrayList<>();
-        printOneSubsequenceOfArray(result2);
-
-        int result3 = countAllSubsequenceOfArray();
-
+//        ArrayList<Integer> result2 = new ArrayList<>();
+//        printOneSubsequenceOfArray(result2);
+//
+//        int result3 = countAllSubsequenceOfArray();
+//        System.out.println(result3);
 
     }
 }
