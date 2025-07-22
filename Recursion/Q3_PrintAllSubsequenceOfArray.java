ea99811af42ce5443ed1552df5fcf790f75db389
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-22 11:44:33 +0530

    Print One Subsquence Of Array

--- Recursion/Q3_PrintAllSubsequenceOfArray.java
+++ Recursion/Q3_PrintAllSubsequenceOfArray.java
@@ -6,9 +6,20 @@ public class Q3_PrintAllSubsequenceOfArray {
     public static ArrayList<ArrayList<Integer>> printAllSubsequenceOfArray(ArrayList<ArrayList<Integer>> res){
         return  res;
     }
+    public static ArrayList<Integer> printOneSubsequenceOfArray(ArrayList<Integer> res){
+        return  res;
+    }
     public static void main(String[] args) {
 
         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
         printAllSubsequenceOfArray(result);
+
+
+        ArrayList<Integer> result2 = new ArrayList<>();
+        printOneSubsequenceOfArray(result2);
+
+
+
+
     }
 }
