a51ca10bdb5808765456d3599c134d5591864d3c
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-07-28 01:01:49 +0530

    Partition array to k subset

--- BackTracking/Q5_PartitionArrayTokSubsets.java
+++ BackTracking/Q5_PartitionArrayTokSubsets.java
@@ -1,7 +1,14 @@
 package BackTracking;
 
 public class Q5_PartitionArrayTokSubsets {
-    public static void main(String[] args) {
 
+    public static String kthPermutation(int n, int k) {
+        // code here
+
+        int total = ;
+    }
+    public static void main(String[] args) {
+      int n=3;
+      int k=3;
     }
 }
---
+++ BackTracking/Q6_PartitionArrayTokSubsets.java
@@ -0,0 +1,7 @@
+package BackTracking;
+
+public class Q6_PartitionArrayTokSubsets {
+    public static void main(String[] args) {
+
+    }
+}
