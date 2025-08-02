d633d1db10e51f89a3ede0964df4108d5feb562a
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-08-02 16:04:10 +0530

    Fibonacci Problem

--- BackTracking/Q6_PartitionArrayTokSubsets.java
+++ BackTracking/Q6_PartitionArrayTokSubsets.java
@@ -4,29 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Q6_PartitionArrayTokSubsets {
-
-    public static void findCombinations(int[] arr, int ind, int target, List<Integer> ds, List<List<Integer>> ans){
-        if (target == 0) {
-            ans.add(new ArrayList<>(ds));
-            return;
-        }
-
-        for (int i = ind; i < arr.length; i++) {
-            if (i > ind && arr[i] == arr[i - 1]) continue;
-            if (arr[i] > target) break;
-
-            ds.add(arr[i]);
-            findCombinations(arr,i + 1, target - arr[i],ds, ans);
-            ds.remove(ds.size() - 1);
-        }
-
-    }
-    public boolean isKPartitionPossible(int[] arr, int k) {
-        // Your code here
-
-    }
-
-    public static void main(String[] args) {
-
-    }
+//
+//    public static void findCombinations(int[] arr, int ind, int target, List<Integer> ds, List<List<Integer>> ans){
+//        if (target == 0) {
+//            ans.add(new ArrayList<>(ds));
+//            return;
+//        }
+//
+//        for (int i = ind; i < arr.length; i++) {
+//            if (i > ind && arr[i] == arr[i - 1]) continue;
+//            if (arr[i] > target) break;
+//
+//            ds.add(arr[i]);
+//            findCombinations(arr,i + 1, target - arr[i],ds, ans);
+//            ds.remove(ds.size() - 1);
+//        }
+//
+//    }
+//    public boolean isKPartitionPossible(int[] arr, int k) {
+//        // Your code here
+//
+//    }
+//
+//    public static void main(String[] args) {
+//
+//    }
 }
--- Dynamic Programing/Q1_Fibonacci.java
+++ Dynamic Programing/Q1_Fibonacci.java
@@ -1,11 +1,29 @@
 public class Q1_Fibonacci {
+
+    public static int fibonacci(int n,int[] dp){
+
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        if(n<=1){
+            return n;
+        }
+        return  dp[n]=fibonacci(n-1,dp)+fibonacci(n-2,dp);
+    }
+
     public static void main(String[] args) {
-        // FIBONACCI
-        // Tabulation
-        // Memoisation
-        // frog run challenge
-        // frog jum
-        // frog jump 2 problem
-        // fubonaccco 2
+
+        int n =10;
+        int[] dp = new int[n+1];
+
+        for(int i=0;i<=n;i++){
+            dp[i]=-1;
+        }
+        int result =fibonacci(n,dp);
+        System.out.println(result);
+//        for(int i=0;i<=n;i++){
+//            System.out.print(dp[i]+" ");
+//        }
+//        System.out.println();
     }
 }
