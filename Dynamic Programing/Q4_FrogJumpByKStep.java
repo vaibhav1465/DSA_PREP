fc153d06da216ead37f5942255d470aded04dfcc
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-08-03 23:48:36 +0530

    house of robbers

---
+++ Dynamic Programing/Q4_FrogJumpByKStep.java
@@ -0,0 +1,39 @@
+public class Q4_FrogJumpByKStep {
+
+    public static int frogJumpByKSteps(int[] heights, int n,int k,int[] dp) {
+
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        if(n<=0){
+            return 0;
+        }
+
+        int max = Integer.MAX_VALUE;
+        for(int i=1;i<=k;i++){
+            if(n>=i) {
+                max = Math.min(max, (frogJumpByKSteps(heights, n - i, k, dp) + Math.abs(heights[n] - heights[n - i])));
+            }
+        }
+        return dp[n]=max;
+    }
+
+    public static void main(String[] args) {
+
+        int[] a = new int[]{10, 5, 20, 0, 15};
+        int k =2;
+        int n = a.length;
+
+        int[] dp = new int[n+1];
+
+        for(int i=0;i<=n;i++){
+            dp[i]=-1;
+        }
+        int result =  frogJumpByKSteps(a,n-1,k,dp);
+        System.out.println(result);
+
+     // house of robbers
+
+    }
+}
