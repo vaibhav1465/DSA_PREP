436c6a79eba0fcacf8ca33f7bc2a501345230c98
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2025-08-08 23:08:13 +0530

    2d array dp

--- Dynamic Programing/Q4_FrogJumpByKStep.java
+++ Dynamic Programing/Q4_FrogJumpByKStep.java
@@ -36,7 +36,7 @@ public class Q4_FrogJumpByKStep {
 
         // house of robbers
         // maxisum sum of non adacent
-        // 2d array questions dp
+        // 2d array questions dp 1
 
     }
 }
